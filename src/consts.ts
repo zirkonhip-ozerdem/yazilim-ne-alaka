8828b7dfdd7e35209110f58c7f3fd68019808b03
Özge Doğan <zirkonhip@gmail.com>
2025-06-20 21:24:08 +0300

    anasayfa ve header footer düzeni sağlandı iskelet oturdu

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'YAZILIM NE ALAKA';
+export const SITE_DESCRIPTION = 'YAZILIM NE ALAKA DİYORUZ ÇÜNKÜ HER ŞEYİN ARKASINDA BİR KOD VAR!';
